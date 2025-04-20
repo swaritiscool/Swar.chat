f3b39ea2373589097c0f476183f463b92bb0974a
Swarit Acharya <swarit.acharya@gmail.com>
2025-04-20 11:34:19 +0530

    Added Copy Functionality

--- frontend/swar.chat/src/components/card.js
+++ frontend/swar.chat/src/components/card.js
@@ -1,4 +1,5 @@
-import { CircularProgress } from "@mui/material";
+"use client";
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
 import styles from "./card.module.css";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -7,6 +8,8 @@ import "highlight.js/styles/felipec.css";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import { CopyAll } from "@mui/icons-material";
+import { useState } from "react";
 
 export const UserCard = ({ content }) => {
   return (
@@ -22,6 +25,21 @@ export const UserCard = ({ content }) => {
 };
 
 export const AICard = ({ content, loaded, model }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(content);
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return loaded === false ? (
     <div className={styles.AICard}>
       <CircularProgress size="30px" />
@@ -35,7 +53,22 @@ export const AICard = ({ content, loaded, model }) => {
       >
         {content}
       </ReactMarkdown>
-      <p className={styles.model}>{model}</p>
+      <div className={styles.details}>
+        <p className={styles.model}>{model}</p>
+        <button className={styles.copy} onClick={handleCopy}>
+          <CopyAll />
+        </button>
+      </div>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Copied text to clipboard
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
