5ae3d9cf5570dc19685e70c9f7eec95a7765fc06
swaritiscool <swarit.acharya@gmail.com>
2025-05-05 18:30:33 +0530

    Changed Routing

---
+++ frontend/swar.chat/src/app/about/page.js
@@ -0,0 +1,48 @@
+"use client";
+
+import styles from "./page.module.css";
+import { Poppins, Space_Mono } from "next/font/google";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { v3 as uuidv4 } from "uuid";
+import Snackbar from "@mui/material/Snackbar";
+import { UserCard, AICard } from "@/components/card";
+import Dropdown from "@mui/joy/Dropdown";
+import Menu from "@mui/joy/Menu";
+import MenuButton from "@mui/joy/MenuButton";
+import MenuItem from "@mui/joy/MenuItem";
+import { Alert, Input, Switch } from "@mui/material";
+import {
+  ArrowCircleDown,
+  ArrowDownward,
+  ArrowDownwardOutlined,
+  ArrowDownwardRounded,
+  KeyboardDoubleArrowDown,
+  KeyboardReturn,
+  Send,
+} from "@mui/icons-material";
+// const fs = require("fs");
+
+const spaceMono = Space_Mono({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div className={spaceMono.className} style={{ height: "99vh" }}>
+      <div className={styles.page}>
+        <div className={styles.centre}>
+          <p className={styles.title}>
+            CHAT WITH LLMS LOCALLY WITH OLLAMA AND SWAR.CHAT!
+          </p>
+          <p className={styles.subtext}>
+            Start chatting with your local LLMs right now at{" "}
+            <a href="/">https://swarchat.vercel.app</a>
+          </p>
+          <p className={styles.supersub}>Made by Swarit Acharya</p>
+        </div>
+      </div>
+    </div>
+  );
+}
